a6e8ee235041a85a73d043a3c2bd22eb2c6fac19
Angelica Siena Abaya <siena.abaya@aestrea.com>
2014-12-16 19:53:44 +0800

    added on click function in work and contact nav button

--- js/crux.js
+++ js/crux.js
@@ -2,7 +2,14 @@ $(function(){
     $(document).on('click','#about-nav', function(event){
         event.preventDefault();
         $('html, body').animate({scrollTop:$('#about').position().top}, 'slow');
-
+    });
+    $(document).on('click','#work-nav', function(event){
+        event.preventDefault();
+        $('html, body').animate({scrollTop:$('#work').position().top}, 'slow');
+    });
+    $(document).on('click','#contact-nav', function(event){
+        event.preventDefault();
+        $('html, body').animate({scrollTop:$('#contact').position().top}, 'slow');
     });
 });
 
