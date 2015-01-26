6e82fc25c56735c04720e79daaacc991df8bc669
Bryan Chug <bryan.chug@gmail.com>
2015-01-26 22:00:00 +0800

    Bundle javascripts

--- js/front.js
+++ js/front.js
@@ -1,3 +1,43 @@
+$(document).ready(function(){
+  $(window).scroll(function(){
+    var brand = $('.brand-logo');
+    var treshold = ($('.item.active').offset().top + $('#gap').offset().top - 100)
+    if ($(window).scrollTop() > treshold ) {
+      if (!brand.hasClass('nav-scrolled')){
+        //brand.fadeOut();
+        brand.attr('src', '/img/crux-sign.png');
+        brand.fadeIn();
+        brand.addClass('nav-scrolled');
+        $('.navbar').addClass('navbar-crux-default').find('.container').css('padding-top', '8px');;
+      }
+    }
+    else if ($(window).scrollTop() < treshold) {
+      brand.attr('src', '/img/crux-logo.png');
+      brand.fadeIn();
+      brand.removeClass('nav-scrolled');
+      $('.navbar').removeClass('navbar-crux-default').find('.container').css('padding-top', '42px');
+    }
+  });
+});
+
+$(function(){
+  $(document).on('click','#about-nav', function(event){
+    event.preventDefault();
+    $('html, body').animate({scrollTop:$('#about').position().top - 45}, 'easeInOutExpo');
+  });
+  $(document).on('click','#work-nav', function(event){
+    event.preventDefault();
+    $('html, body').animate({scrollTop:($('.item.active').offset().top + $('#gap').offset().top - 72)}, 'slow');
+  });
+  $(document).on('click','#contact-nav', function(event){
+    event.preventDefault();
+    $('html, body').animate({scrollTop:$('#contact').position().top}, 'slow');
+  });
+  $(document).on('click','#home-nav', function(event){
+    event.preventDefault();
+    $('html, body').animate({scrollTop:$('#home').position().top}, 'slow');
+  });
+});
 
    var tag = document.createElement('script');
     console.log('HERE')
@@ -18,7 +58,7 @@
             modestBranding: 0,
             loop: 1,
             rel: 0,
-            info: 0 
+            info: 0
           },
           events: {
             'onReady': onPlayerReady
@@ -46,6 +86,6 @@ $(document).ready(function(){
     $('.carousel').on('slid.bs.carousel', function(){
         console.log("WE SLID!")
         customPlayback();
-    }); 
+    });
 
 });
