59e06dc7f5aec8e73a20c753f11a49e957ac1437
Jerico Caesar Delfinado <jerico.delfinado@gmail.com>
2015-03-04 19:22:09 +0800

    Update nav

--- js/front.js
+++ js/front.js
@@ -1,21 +1,20 @@
 $(document).ready(function(){
   $(window).scroll(function(){
-    var brand = $('.brand-logo');
+    var $brand = $('.navbar-brand');
+    var $logo = $brand.find('.nav-logo');
     var treshold = ($('.carousel-inner').outerHeight()-100);
     if ($(window).scrollTop() > treshold ) {
-      if (!brand.hasClass('nav-scrolled')){
-        //brand.fadeOut();
-        brand.attr('src', '/img/crux-sign.png');
-        brand.fadeIn();
-        brand.addClass('nav-scrolled');
-        $('.navbar').addClass('navbar-crux-default').find('.container').css('padding-top', '8px');;
+      if (!$brand.hasClass('nav-scrolled')){
+        
+        $brand.addClass('nav-scrolled');
+        $logo.attr('src', '/img/crux-logo-inverse.png');
+        $('.navbar').addClass('navbar-crux-default');
       }
     }
     else if ($(window).scrollTop() < treshold) {
-      brand.attr('src', '/img/crux-logo.png');
-      brand.fadeIn();
-      brand.removeClass('nav-scrolled');
-      $('.navbar').removeClass('navbar-crux-default').find('.container').css('padding-top', '42px');
+      $logo.attr('src', '/img/crux-brand-inverse.png');
+      $brand.removeClass('nav-scrolled');
+      $('.navbar').removeClass('navbar-crux-default');
     }
   });
 });
