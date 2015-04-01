62547daa4dbf4dca71171dc84546e7c64d1534b3
Angelica Siena Abaya <siena.abaya@aestrea.com>
2015-04-01 16:06:01 +0800

    fix mobile view

--- js/front.js
+++ js/front.js
@@ -3,18 +3,20 @@ $(document).ready(function(){
         var $brand = $('.navbar-brand');
         var $logo = $brand.find('.nav-logo');
         var treshold = ($('#cover').outerHeight()-34);
-        if ($(window).scrollTop() > treshold ) {
-            if (!$brand.hasClass('nav-scrolled')){
+        if(window.innerWidth >= 768){
+            if ($(window).scrollTop() > treshold ) {
+                if (!$brand.hasClass('nav-scrolled')){
 
-                $brand.addClass('nav-scrolled');
-                $logo.attr('src', '/img/crux-logo-inverse.png');
-                $('.navbar').addClass('navbar-crux-default');
+                    $brand.addClass('nav-scrolled');
+                    $logo.attr('src', '/img/crux-logo-inverse.png');
+                    $('.navbar').addClass('navbar-crux-default');
+                }
             }
-        }
-        else if ($(window).scrollTop() < treshold) {
-            $logo.attr('src', '/img/crux-brand-inverse.png');
-            $brand.removeClass('nav-scrolled');
-            $('.navbar').removeClass('navbar-crux-default');
+            else if ($(window).scrollTop() < treshold) {
+                $logo.attr('src', '/img/crux-brand-inverse.png');
+                $brand.removeClass('nav-scrolled');
+                $('.navbar').removeClass('navbar-crux-default');
+            }
         }
     });
 });
